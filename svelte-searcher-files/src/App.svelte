691274dcafb02bafc16fe0e4a7f7b6e108e06421
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-01-29 13:00:34 +0000

    Added suggestions reactive variable.

--- svelte-searcher-files/src/App.svelte
+++ svelte-searcher-files/src/App.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
-    export let name: string
+    import { onMount } from 'svelte'
+
+    let pokemonData: string[] = []
+    const getPokemonData = async (): Promise<void> => {
+        const rawPokemonData = await (
+            await fetch('https://pokeapi.co/api/v2/pokemon?limit=99')
+        ).json()
+        pokemonData = rawPokemonData.results.map(
+            (p: { name: string; url: string }) => p.name
+        )
+    }
+
+    // onMount(async () => {
+    //     pokemonData = await getPokemonData()
+    // })
+
+    let pokemonName: string = ''
+
+    let suggestions: string[]
+    $: suggestions =
+        pokemonName.length > 0
+            ? pokemonData.filter((v) =>
+                  new RegExp(`^${pokemonName}`, `i`).test(v)
+              )
+            : pokemonData
 </script>
 
 <main>
-    <h1>Hello {name}!</h1>
-    <p>
-        Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-        how to build Svelte apps.
-    </p>
+    <h1>Search</h1>
+    <input type="text" bind:value="{pokemonName}" />
 </main>
 
 <style>
@@ -23,6 +44,10 @@
         text-transform: uppercase;
         font-size: 4em;
         font-weight: 100;
+    }
+
+    li {
+        font-size: 24px;
     }
 
     @media (min-width: 640px) {
