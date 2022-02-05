3a9e9550a9581161a22ebb569dd59606141b36d1
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-02-05 19:15:24 +0000

    Added suggestions component.

---
+++ svelte-searcher-files/src/Suggestion.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte'
+
+    export let suggestion: string
+    export let chosenPokemon2: string = ''
+    const dispatch = createEventDispatcher()
+
+    const chosePokemon = (): void => {
+        dispatch('chosePokemon', {
+            pokemon: suggestion,
+        })
+
+        chosenPokemon2 = suggestion
+    }
+</script>
+
+<div class="suggestion" on:click="{chosePokemon}">{suggestion}</div>
+
+<style>
+    .suggestion {
+        font-size: 1.25rem;
+    }
+</style>
