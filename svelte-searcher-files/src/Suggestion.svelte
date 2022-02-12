a4d417110b6cf4d7e23ceb19a3a06820be4dd78e
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-02-12 17:29:23 +0000

    Removed custom event dispatch from suggestion

--- svelte-searcher-files/src/Suggestion.svelte
+++ svelte-searcher-files/src/Suggestion.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
-    import { createEventDispatcher } from 'svelte'
-
     export let suggestion: string
-    export let chosenPokemon2: string = ''
-    const dispatch = createEventDispatcher()
-
-    const chosePokemon = (): void => {
-        dispatch('chosePokemon', {
-            pokemon: suggestion,
-        })
-
-        chosenPokemon2 = suggestion
-    }
+    export let chosenPokemon: string = ''
 </script>
 
-<div class="suggestion" on:click="{chosePokemon}">{suggestion}</div>
+<div class="suggestion" on:click="{() => (chosenPokemon = suggestion)}">
+    {suggestion}
+</div>
 
 <style>
     .suggestion {
